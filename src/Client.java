f9c6cb6402c669ccbdef357ae7f4742d1cc8060f
Elmira <elmira.aras@protonmail.com>
2023-12-22 01:54:12 +0300

    methodize client

--- src/Client.java
+++ src/Client.java
@@ -3,34 +3,34 @@ import java.net.Socket;
 
 public class Client {
 
+    public static void sendData(String serverAddress, int serverPort, String filePath){
+        try(Socket socket = new Socket(serverAddress, serverPort); OutputStream outputStream = socket.getOutputStream(); BufferedInputStream fileInput = new BufferedInputStream(new FileInputStream(filePath))){
+            // send the file name to the server
+            String fileName = new File(filePath).getName();
+            byte[] fileNameBytes = fileName.getBytes();
+            outputStream.write(fileNameBytes.length);
+            outputStream.write(fileNameBytes);
+
+            // buffer for reading data
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            // read data from the file and send it to the server
+            while((bytesRead = fileInput.read(buffer)) != -1){
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println(fileName + " sent successfully!");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         String serverAddress = "212.133.250.63"; // Replace with the actual IP address of the server
         int serverPort = 3000;
         String filePath = "C:\\Users\\E\\Downloads\\ALIZADE-SU-AN.mp3";
 
-        try (Socket socket = new Socket(serverAddress, serverPort);
-             OutputStream out = socket.getOutputStream();
-             BufferedInputStream fileIn = new BufferedInputStream(new FileInputStream(filePath))) {
-
-            // Send the file name to the server
-            String fileName = new File(filePath).getName();
-            byte[] fileNameBytes = fileName.getBytes();
-            out.write(fileNameBytes.length);
-            out.write(fileNameBytes);
-
-            // Buffer for reading data
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-
-            // Read data from the file and send it to the server
-            while ((bytesRead = fileIn.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-
-            System.out.println("File sent successfully.");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        sendData(serverAddress, serverPort, filePath);
     }
 }
